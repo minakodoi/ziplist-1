92db5a718ab9599705f21fee8221adb950ca886b
Minako Doi <minakod@hawaii.edu>
2016-09-19 14:31:53 -1000

    Updated from desktop

---
+++ ziplist.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Minako on 2016/09/19.
+ */
+
+const test1 = ['a', 'b', 'c'];
+const test2 = [1, 2, 3];
+
+function zipList(list1, list2)
+{
+  let combine = [];
+  for (let i = 0; i < list1.length; i++)
+  {
+    combine.push(list1[i]);
+    combine.push(list2[i]);
+  }
+  return combine;
+}
+
+function zipListTheSimpleWay(list1, list2)
+{
+  const ziplist = _.zip(list1, list2);
+  return _.flatten(ziplist);
+}
+
+console.log(zipList(test1, test2));
+
+console.log(zipListTheSimpleWay(test1, test2));
