b209c4426c12b67565e4194d54d64c525ea4cffd
Minako Doi <minakod@hawaii.edu>
2016-09-19 19:34:43 -1000

    Updated from desktop

--- ziplist.js
+++ ziplist.js
@@ -5,19 +5,16 @@
 const test1 = ['a', 'b', 'c'];
 const test2 = [1, 2, 3];
 
-function zipList(list1, list2)
-{
-  let combine = [];
-  for (let i = 0; i < list1.length; i++)
-  {
+function zipList(list1, list2) {
+  const combine = [];
+  for (let i = 0; i < list1.length; i + 1) {
     combine.push(list1[i]);
     combine.push(list2[i]);
   }
   return combine;
 }
 
-function zipListTheSimpleWay(list1, list2)
-{
+function zipListTheSimpleWay(list1, list2) {
   const ziplist = _.zip(list1, list2);
   return _.flatten(ziplist);
 }
